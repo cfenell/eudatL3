c984d06f45538bf27541a9b12588cd91c1d9243f
Carl-Fredrik Enell <carl-fredrik.enell@eiscat.se>
2018-03-06 13:05:53 +0000

    Working copy, near functioning

--- modules/madSearch.py
+++ modules/madSearch.py
@@ -1,73 +1,68 @@
-### Import configuration
-
-
 ### Search an instance of Madrigal for experiments
 ### and create B2SHARE records
-class madSearch:
-
-    def __init__(self):
-
-        from configparser import SafeConfigParser
-        from B2SHAREClient import B2SHAREClient,EISCATmetadata
-        import madrigalWeb.madrigalWeb as mw
-
-        ## Read config
-        config=SafeConfigParser()
-        config.read('/usr/local/etc/eudatL2.conf')        
-        madurl=self.config.get('Madrigal','URL')
-
-        ## Open Madrigal connection
-        try:
-            self.madData=mw.MadrigalData(madurl)
-        except:
-            raise IOError("Could not open Madrigal connection")
-
-        ## Set up one B2SHARE client instance 
-        client=B2SHAREClient.B2SHAREClient(community_id=self.config.get('B2','community'), url=self.config.get('B2','b2share_url'),token=self.config.get('B2','token') )
-
     
+def B2entries(insts, byear, bmonth, bday, bhour, bmin, bsec, eyear, emonth, eday, ehour, emin, esec):
     
-    def B2entries(self, insts, byear, bmonth, bday, bhour, bmin, bsec, eyear, emonth, eday, ehour, emin, esec):
+
+    from configparser import SafeConfigParser
+    from B2SHAREClient import EISCATmetadata, B2SHAREClient
+    import madrigalWeb.madrigalWeb as mw
+    import datetime
         
-        import datetime
+    ## Read config
+    config=SafeConfigParser(inline_comment_prefixes={'#'})
+    config.read('/usr/local/etc/eudatL2.conf')        
+    madurl=config.get('Madrigal','URL')
+    
+    ## Open Madrigal connection
+    try:
+        madData=mw.MadrigalData(madurl)
+    except:
+        raise IOError("Could not open Madrigal connection")
+    
+    ## Set up one B2SHARE client instance 
+    client=B2SHAREClient.B2SHAREClient(community_id=config.get('B2','community'), url=config.get('B2','b2share_url'),token=config.get('B2','token') )
+
         
-        ## List experiments
-        madExps=self.madData.getExperiments(insts, byear, bmonth, bday, bhour, bmin, bsec, eyear, emonth, eday, ehour, emin, esec, local=1)
+    ## List experiments
+    madExps=madData.getExperiments(insts, byear, bmonth, bday, bhour, bmin, bsec, eyear, emonth, eday, ehour, emin, esec, local=1)
         
-        ## Main loop over Madrigal experiments
-        for thisExp in madExps:
+    ## Main loop over Madrigal experiments
+    for thisExp in madExps:
 
-            ## Build argument list for EISCATmetadata
-            args=[]
-            args.append(thisExp.id) # Experiment ID
-            args.append(thisExp.name))    # Experiment name
-
-            instMap={71:"kir", 72:"uhf", 73:"sod", 74:"vhf", 75: "kir", 76: "sod",  95:"lyr" }
-            antenna=instMap(thisExp.instcode)
-            args.append(antenna) # Instrument
-
-            args.append(None) # Resources
-
-            startTime=datetime.datetime(thisExp.startyear, thisExp.startmonth, thisExp.startday, thisExp.starthour, thisExp.startmin, thisExp.startsec)
-            args.append(startTime) # Start time
-            
-            endTime=datetime.datetime(thisExp.endyear, thisExp.endmonth, thisExp.endday, thisExp.endhour, thisExp.endmin, thisExp.endsec)
-            args.append(endTime)
-
-            args.append(None) #Embargo time
-            args.append(None) # Info directory
-
-            ## Create metadata
-            metadata_json = self.EISCATmetadata.MetaDataJSON(args, thisExp.realUrl , self.config.get('B2','community'), self.config.get('B2','community_specific'))
-
-            ## Create B2SHARE draft
-            draft_json=self.client.create_draft(metadata_json)
-
-            ## get the files: plots, hdf5
-            ## ... todo
-            
-            ## Upload file(s) to draft
-            bucketID=draft_json['links']['files']
-            # client.put_draft_file(bucketID, [ outFile ])
-            
-            
+        print(thisExp)
+        ## Build argument list for EISCATmetadata
+        args=[]
+        args.append(thisExp.id) # Experiment ID
+        
+        args.append(thisExp.name)    # Experiment name
+        
+        instMap={71:"kir", 72:"uhf", 73:"sod", 74:"vhf", 75: "kir", 76: "sod",  95:"lyr" }
+        antenna=instMap[thisExp.instcode]
+        args.append(antenna) # Instrument
+        
+        args.append(None) # Resources
+        
+        startTime=datetime.datetime(thisExp.startyear, thisExp.startmonth, thisExp.startday, thisExp.starthour, thisExp.startmin, thisExp.startsec)
+        args.append(startTime) # Start time
+        
+        endTime=datetime.datetime(thisExp.endyear, thisExp.endmonth, thisExp.endday, thisExp.endhour, thisExp.endmin, thisExp.endsec)
+        args.append(endTime)
+        
+        args.append(None) #Embargo time
+        args.append(None) # Info directory
+        
+        ## Create metadata
+        metadata_json = EISCATmetadata.MetaDataJSON(args, thisExp.realUrl , config.get('B2','community'), config.get('B2','community_specific'))
+        
+        ## Create B2SHARE draft
+        draft_json=client.create_draft(metadata_json)
+        
+        ## get the files: plots, hdf5
+        ## ... todo
+        
+        ## Upload file(s) to draft
+        bucketID=draft_json['links']['files']
+        # client.put_draft_file(bucketID, [ outFile ])
+        print(bucket_id)
+        
--- scripts/L3toB2.py
+++ scripts/L3toB2.py
@@ -9,132 +9,67 @@ Search Madrigal server instance for data and create B2SHARE entries.
 carl-fredrik.enell@eiscat.se
 """
 
-import os,  sys
-import traceback
-import getopt
-import madrigal.metadata
-from time import strptime, struct_time, sleep
+import sys, getopt
+from time import strptime
+from madSearch import B2entries
 
 
 if __name__ == '__main__':
     
-    
-    
-    
-### Define help string
-usage = """[--inst=<comma-separated-kinsts>] [--newer=<yyyy-mm-ddTHH:MM:SS>]
+    ### Define help string
+    usage = """[--inst=<comma-separated-kinsts>] <start-time> <end-time>
 
---inst - to specify what instruments to include (comma separated kinsts). Default is all Eiscat instruments.
---newer=<yyyy-mm-ddTHH:MM:SS>: to create output only for experiments newer than specified time.
-"""
+    --inst - to specify what instruments to include (comma separated kinsts). Default is all Eiscat instruments.
+    """
 
-
-### Set default arguments
-# Instruments: all EISCAT instruments.
-# Modify this for your site
-instrumentList = [70,71,72,73,74,75,76,95,2950,2951]
-
-# Default start time: 1970-01-01
-fromTime=strptime('1970-01-01T00:00:00','%Y-%m-%dT%H:%M:%S')
-
-
-### Parse command line
-
-try:
-    opts, args = getopt.getopt(sys.argv[1:], "", ["inst=","newer="])
-except getopt.GetoptError, err:
-    print str(err) 
-    print(usage)
-    sys.exit(2)
-
-if len(args) != 1:
-    print(usage)
-    sys.exit(-1)
-
-
-for o, a in opts:
-    if o == "--inst":
-        instList = a.split(',')
-        instrumentList = []
-        for item in instList:
-            try:
-                instrumentList.append(int(item))
-            except NameError:
-                raise ValueError, 'Unable to parse %s - should be comma separated kinst values (int)' % (a)
-
-    if o == "--newer":
-        try:
-            fromTime=strptime(a,'%Y-%m-%dT%H:%M:%S')
-        except ValueError:
-            print("newer: Wrong date format: specify date as yyyy-mm-ddTHH:MM:SS")
-            sys.exit(1)
-
+    ### Default instruments: all EISCAT radars
+    instrumentList = [70,71,72,73,74,75,76,95]
+        
+    ### Parse command line
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "", ["inst="])
+    except getopt.GetoptError as err:
+        print(str(err))
+        print(usage)
+        sys.exit(2)
+        
+    if len(args) != 2:
+        print(usage)
+        sys.exit(-1)
             
-    if o == "--delete":
-        delXML=1
-        try:
-            xmlDir=a
-        except ValueError:
-            raise ValueError, "Give directory where existing Observation XML files can be found, --delete=<dir>"
 
-print 'Creating B2SHARE records for instrument id :',
-for inst in instrumentList:
-          print '%d' %inst,
-print
+    for o, a in opts:
 
+        if o == "--inst":
+            instList = a.split(',')
+            instrumentList = []
+            for item in instList:
+                try:
+                    instrumentList.append(int(item))
+                except NameError:
+                    raise ValueError("Unable to parse %s - should be comma separated kinst values (int)' % (a)")
 
-### Set up needed Madrigal metadata objects
-madDB = madrigal.metadata.MadrigalDB()
-siteID = madDB.getSiteID()
-madExp = madrigal.metadata.MadrigalExperiment(madDB)
-madInst = madrigal.metadata.MadrigalInstrument(madDB)
+    try:
+        startTime=strptime(args[0],'%Y-%m-%dT%H:%M:%S')
+    except:
+        raise ValueError("Wrong start time format")
 
-
-### Main loop over Madrigal experiments
-for i in range(madExp.getExpCount()):
-
-    # Ignore experiments with start time older than or equal to fromTime.
-    if struct_time(madExp.getExpStartDateTimeByPosition(i))[0:6] <= fromTime[0:6]:
-        continue
+    try:
+        endTime=strptime(args[1],'%Y-%m-%dT%H:%M:%S')
+    except:
+        raise ValueError("Wrong start time format")
     
-    thisSiteID = madExp.getExpSiteIdByPosition(i)    
+    # Todo: change me to logging                                
+    print("Creating B2SHARE records for instruments :" + str(instrumentList)),
+    
 
-    # Ignore non-local experiments
-    if thisSiteID != siteID:
-        continue
 
-    kinst = madExp.getKinstByPosition(i)
+    ### Call Madrigal-B2SHARE routine
+    b2create=B2entries(instrumentList, startTime.tm_year, startTime.tm_mon, startTime.tm_mday, startTime.tm_hour, startTime.tm_min, startTime.tm_sec, endTime.tm_year, endTime.tm_mon, endTime.tm_mday, endTime.tm_hour, endTime.tm_min, endTime.tm_sec)
 
-    # Ignore instruments not in list
-    if instrumentList:
-        if kinst not in instrumentList:
-            continue
-
-    # Ignore non-ISR data
-    # Comment out if not @EISCAT
-    #if madInst.getCategoryId(kinst) != 1:
-    #    continue 
-       
-    expId = madExp.getExpIdByPosition(i)
-    expDir = madExp.getExpDirByPosition(i)
-  
-    # Ignore test experiments
-    if madDB.isTestExperiment(expDir):
-        continue
     
     
-    # Conditions are passed -  Create record
-    print('Working on exp no: %d id: %d path: %s' % (i,expId,expDir))
+
+
+
     
-    # Deletion entry
-    if(delXML):
-        print('Creating Deletion entry from exp no %d in %s' % (expId,xmlDir))
-        obj=delObs(expId,xmlDir,outputDir)
-        del(obj)
-
-    # New Observation entry
-    obj=Observation(outputDir,expId)
-    del(obj)
-    sleep(.25)
-
-### EOF
