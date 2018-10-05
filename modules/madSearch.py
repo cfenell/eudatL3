7683a5f9f0919b06f8ff4ac6725e1d9b232a2058
Carl-Fredrik Enell <carl-fredrik.enell@eiscat.se>
2018-10-05 14:34:19 +0000

    Added logging Added simple check to exclude realtime (Todo: check if all realtime-only cases are covered ie multiple realtime files? )

--- modules/madSearch.py
+++ modules/madSearch.py
@@ -3,43 +3,67 @@
     
 def B2entries(insts, byear, bmonth, bday, bhour, bmin, bsec, eyear, emonth, eday, ehour, emin, esec):
     
-
-    from configparser import SafeConfigParser
     from B2SHAREClient import EISCATmetadata, B2SHAREClient
     import madrigalWeb.madrigalWeb as mw
     import madPlots
     import datetime
     from os import path, scandir, unlink
-        
+    from configparser import SafeConfigParser
+    import logging
+
+    ## Hardcoded numbering, Get from madrigal metadata instead?
+    instMap={71:"kir", 72:"uhf", 73:"sod", 74:"vhf", 75: "kir", 76: "sod",  95:"lyr" }
+    
     ## Read config
     config=SafeConfigParser(inline_comment_prefixes={'#'})
     config.read('/usr/local/etc/eudat.conf')        
     tmpdir=config.get('Main','tempDir')
     madurl=config.get('Madrigal','URL')
-    
+
     ## Open Madrigal connection
+    logging.info("Opening connection to %s" % madurl)
     try:
         madData=mw.MadrigalData(madurl)
     except:
         raise IOError("Could not open Madrigal connection")
     
-    ## Set up one B2SHARE client instance 
-    client=B2SHAREClient.B2SHAREClient(community_id=config.get('B2','community'), url=config.get('B2','b2share_url'),token=config.get('B2','token') )
-
+    ## Set up one B2SHARE client instance
+    b2url=config.get('B2','b2share_url')
+    logging.info("Connecting to B2SHARE instance at %s" % b2url)
+    try:
+        client=B2SHAREClient.B2SHAREClient(community_id=config.get('B2','community'), url=b2url, token=config.get('B2','token'))
+    except:
+        raise IOError("Could not open B2Share connection")
         
     ## List experiments
-    madExps=madData.getExperiments(insts, byear, bmonth, bday, bhour, bmin, bsec, eyear, emonth, eday, ehour, emin, esec, local=1)
+    try:
+        logging.debug("Listing Madrigal experiments")
+        madExps=madData.getExperiments(insts, byear, bmonth, bday, bhour, bmin, bsec, eyear, emonth, eday, ehour, emin, esec, local=1)
+    except:
+        raise IOError("Could not list Madrigal experiments")
         
     ## Main loop over Madrigal experiments
     for thisExp in madExps:
+        
+        ## get the list of NCAR files
+        logging.debug("Working on %s" % str(thisExp.id))
+        expFiles=madData.getExperimentFiles(thisExp.id)
 
+        ## Check for realtime experiments
+        if len(expFiles) == 1:
+            if (expFiles[0].category == 4) or expFiles[0].name.endswith('.asc') :
+                # only a realtime file, skip to next experiment
+                logging.info("Skipping experiment %s: only one realtime file" % str(thisExp.id))
+                continue       
+
+        logging.debug("Continuing with experiment %s" % str(thisExp.id))
         ## Build argument list for EISCATmetadata
         args=[]
         args.append(thisExp.id) # Experiment ID
         
         args.append(thisExp.name)    # Experiment name
         
-        instMap={71:"kir", 72:"uhf", 73:"sod", 74:"vhf", 75: "kir", 76: "sod",  95:"lyr" }
+
         antenna=instMap[thisExp.instcode]
         args.append(antenna) # Instrument
         
@@ -55,40 +79,73 @@ def B2entries(insts, byear, bmonth, bday, bhour, bmin, bsec, eyear, emonth, eday
         args.append(None) # Info directory
         
         ## Create metadata
+        logging.debug("Creating JSON object")
         metadata_json = EISCATmetadata.MetaDataJSON(args, 3, thisExp.realUrl , config.get('B2','community'), config.get('B2','community_specific'))
         
         ## Create B2SHARE draft
-        draft_json=client.create_draft(metadata_json)
-        
-        ## get the list of NCAR files
-        expFiles=madData.getExperimentFiles(thisExp.id)
+        logging.info("Creating B2SHARE draft record for experiment %s" % str(thisExp.id))
+        try:
+            draft_json=client.create_draft(metadata_json)
+        except:
+            raise IOError("B2share draft creation error")
 
+        ## Upload the files
+        logging.debug("Uploading experiment files")
         for expFile in expFiles:
 
             if expFile.category==1:
                 outFile=path.join(tmpdir, path.splitext(path.basename(expFile.name))[0] + '.hdf5')
-            
-                madData.downloadFile(expFile.name, outFile, 'B2Share client', 'b2@eiscat.se', 'EISCAT Scientific Association', format='hdf5')
-                client.put_draft_file(draft_json, [ outFile ])
 
+                logging.info("Getting Madrigal file as %s" % outFile)
+                try:
+                    madData.downloadFile(expFile.name, outFile, 'B2Share client', 'b2@eiscat.se', 'EISCAT Scientific Association', format='hdf5')
+                except:
+                    raise IOError("Could not download experiment file from Madrigal")
+
+                logging.info("Uploading the file to B2share file bucket")
+                try:
+                    client.put_draft_file(draft_json, [ outFile ])
+                except:
+                    raise IOError("Could not upload file")
+                    
                 # Add the parameters
-                expPars=madData.getExperimentFileParameters(expFile.name)
+                logging.debug("Getting parameters in file %s" % expFile.name)
+                try:
+                    expPars=madData.getExperimentFileParameters(expFile.name)
+                except:
+                    raise IOError("Could not get parameters from Madrigal")
+
+                logging.debug("Creating JSON Patch with parameters")
                 param_json_patch=EISCATmetadata.ParamJSONpatch(expPars, config.get('B2','community_specific'))
               
-                # todo: debug this patch
-                client.update_draft(draft_json, param_json_patch)
-                
+                # Patch B2Share entry
+                logging.info("Inserting JSON patch with parameters in B2Share entry")
+                try:
+                    client.update_draft(draft_json, param_json_patch)
+                except:
+                    raise IOError("Could not upload JSON patch")
+                    
         ## Add plots
         # Build true experiment URL
+        logging.debug("Searching for figures to upload")
         expurl=thisExp.url
         if(expurl.find('cgi-bin/madtoc') > 0):
             expurl=expurl.replace('cgi-bin/madtoc/','')
-            
-        plotFiles=madPlots.get_plots(expurl, tmpdir)        
-        if(len(plotFiles)>0):
-            client.put_draft_file(draft_json, plotFiles)       
 
+        try:
+            plotFiles=madPlots.get_plots(expurl, tmpdir)
+        except:
+            raise IOError("Could not search for madrigal plots")
+        
+        if(len(plotFiles)>0):
+            logging.info("Uploading plots to B2Share: %s" % str(plotFiles))
+            try:
+                client.put_draft_file(draft_json, plotFiles)       
+            except:
+                raise IOError("Could not upload the files")
+                
         ## Clean up temp dir
+        logging.debug("Cleaning up files in %s" % tmpdir)
         for tmpfile in scandir(tmpdir):
             unlink(tmpfile.path)
     
--- scripts/L3toB2.py
+++ scripts/L3toB2.py
@@ -12,10 +12,21 @@ carl-fredrik.enell@eiscat.se
 import sys, getopt
 from time import strptime
 from madSearch import B2entries
+from configparser import SafeConfigParser
+import logging
+
+# import configuration
+configuration=SafeConfigParser()
+configuration.read('/usr/local/etc/eudat.conf')
+
+# config log file
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', filename=configuration.get\
+                    ('Log','log_file_path'), level=eval(configuration.get('Log','logging_level')))
 
 
 if __name__ == '__main__':
-    
+
+    logging.info("Entering L3toB2 program")
     ### Define help string
     usage = """[--inst=<comma-separated-kinsts>] <start-time> <end-time>
 
@@ -45,10 +56,11 @@ if __name__ == '__main__':
             instrumentList = []
             for item in instList:
                 try:
+                    logging.debug("Appending %s to instrument list" % item)
                     instrumentList.append(int(item))
                 except NameError:
                     raise ValueError("Unable to parse %s - should be comma separated kinst values (int)' % (a)")
-
+                
     try:
         startTime=strptime(args[0],'%Y-%m-%dT%H:%M:%S')
     except:
@@ -59,14 +71,12 @@ if __name__ == '__main__':
     except:
         raise ValueError("Wrong start time format")
     
-    # Todo: change me to logging                                
-    print("Creating B2SHARE records for instruments :" + str(instrumentList)),
-    
     ### Call Madrigal-B2SHARE routine
+    logging.info("Creating B2SHARE records for instruments :" + str(instrumentList))
+
     b2create=B2entries(instrumentList, startTime.tm_year, startTime.tm_mon, startTime.tm_mday, startTime.tm_hour, startTime.tm_min, startTime.tm_sec, endTime.tm_year, endTime.tm_mon, endTime.tm_mday, endTime.tm_hour, endTime.tm_min, endTime.tm_sec)
 
-    
-    
+    logging.info("L3toB2 done")
 
 
 
